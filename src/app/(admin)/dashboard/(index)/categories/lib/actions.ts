84b0ba1a1a89843497d66121f413b3124cb31d07
adipsagala <apros.pro98@gmail.com>
2024-11-23 18:00:03 +0700

    Add Full Function Category

---
+++ src/app/(admin)/dashboard/(index)/categories/lib/actions.ts
@@ -0,0 +1,98 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { schemaCategory } from "@/lib/schema";
+import { ActionResult } from "@/types";
+import { Action } from "@prisma/client/runtime/library";
+import { redirect } from "next/navigation";
+import { number, unknown } from "zod";
+
+export async function postCategory(
+  _: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validate = schemaCategory.safeParse({
+    name: formData.get("name"),
+  });
+
+  if (!validate.success) {
+    return {
+      error: validate.error.errors[0].message,
+    };
+  }
+
+  try {
+    await prisma.category.create({
+      data: {
+        name: validate.data.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Failed to insert data",
+    };
+  }
+
+  return redirect("/dashboard/categories");
+}
+
+export async function updateCategory(
+  _: unknown,
+  formData: FormData,
+  id: number | undefined
+): Promise<ActionResult> {
+  const validate = schemaCategory.safeParse({
+    name: formData.get("name"),
+  });
+
+  if (!validate.success) {
+    return {
+      error: validate.error.errors[0].message,
+    };
+  }
+
+  if (id === undefined) {
+    return {
+      error: "ID is not found",
+    };
+  }
+
+  try {
+    await prisma.category.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: validate.data.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Failed to update data",
+    };
+  }
+
+  return redirect("/dashboard/categories");
+}
+
+export async function deleteCategory(
+  _: unknown,
+  formData: FormData,
+  id: number
+): Promise<ActionResult> {
+  try {
+    await prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Failed to delete data",
+    };
+  }
+  return redirect("/dashboard/categories");
+}
